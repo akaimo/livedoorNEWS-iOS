79534dc6f151ccd30fcfcbc9cf9f78f73361930c
shuhei <kawashu0723@gmail.com>
2015-06-12 20:47:25 +0900

    Initial Commit

---
+++ livedoorNEWS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  livedoorNEWS
+//
+//  Created by akaimo on 2015/06/12.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ livedoorNEWS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  livedoorNEWS
+//
+//  Created by akaimo on 2015/06/12.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
