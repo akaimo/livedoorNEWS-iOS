6dedce1c011194c696acd5971fa3b18f001cb135
shuhei <kawashu0723@gmail.com>
2015-06-13 13:16:27 +0900

    add fetchData

---
+++ livedoorNEWS/AKAFetchData.h
@@ -0,0 +1,13 @@
+//
+//  AKAFetchData.h
+//  livedoorNEWS
+//
+//  Created by akaimo on 2015/06/13.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AKAFetchData : NSObject
+
+@end
---
+++ livedoorNEWS/AKAFetchData.m
@@ -0,0 +1,13 @@
+//
+//  AKAFetchData.m
+//  livedoorNEWS
+//
+//  Created by akaimo on 2015/06/13.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import "AKAFetchData.h"
+
+@implementation AKAFetchData
+
+@end
