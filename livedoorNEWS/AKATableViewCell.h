b87791af1db8d8e1f2a647827866994c945a0a42
shuhei <kawashu0723@gmail.com>
2015-06-14 22:12:47 +0900

    display dateTime, categoryView's cell height is dynamic

---
+++ livedoorNEWS/AKATableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  AKATableViewCell.h
+//  livedoorNEWS
+//
+//  Created by akaimo on 2015/06/14.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKATableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
---
+++ livedoorNEWS/AKATableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AKATableViewCell.m
+//  livedoorNEWS
+//
+//  Created by akaimo on 2015/06/14.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import "AKATableViewCell.h"
+
+@implementation AKATableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
