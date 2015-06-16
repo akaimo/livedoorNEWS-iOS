b9202a26114bb06815c0823ebc86c0fe9c162124
shuhei <kawashu0723@gmail.com>
2015-06-16 12:44:22 +0900

    add pod 'JDStatusBarNotification'

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// JDStatusBarNotification
+#define COCOAPODS_POD_AVAILABLE_JDStatusBarNotification
+#define COCOAPODS_VERSION_MAJOR_JDStatusBarNotification 1
+#define COCOAPODS_VERSION_MINOR_JDStatusBarNotification 5
+#define COCOAPODS_VERSION_PATCH_JDStatusBarNotification 2
+
 // MBProgressHUD
 #define COCOAPODS_POD_AVAILABLE_MBProgressHUD
 #define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
