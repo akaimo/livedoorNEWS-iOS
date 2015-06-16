f21cca7b61c9679278f836d7a5e4239436da279e
shuhei <kawashu0723@gmail.com>
2015-06-16 23:08:05 +0900

    add back/nextBtn, and refreshBtn

--- livedoorNEWS/AKARssWebViewController.m
+++ livedoorNEWS/AKARssWebViewController.m
@@ -11,6 +11,14 @@
 @interface AKARssWebViewController () <UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *rssWebView;
 
+@property (strong, nonatomic) UIBarButtonItem *actionBtn;
+@property (strong, nonatomic) UIBarButtonItem *refreshBtn;
+@property (strong, nonatomic) UIBarButtonItem *stopBtn;
+@property (strong, nonatomic) UIBarButtonItem *forwardBtn;
+@property (strong, nonatomic) UIBarButtonItem *backBtn;
+
+@property (nonatomic) int webViewLoads;
+
 @end
 
 @implementation AKARssWebViewController
@@ -22,6 +30,21 @@
     NSURL *url = [NSURL URLWithString:_url];
     NSLog(@"%@", _url);
     [_rssWebView loadRequest:[NSURLRequest requestWithURL:url]];
+    
+    _actionBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionBtnTap:)];
+    _refreshBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshBtnTap:)];
+    _stopBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(stopBtnTap:)];
+    _forwardBtn = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"nextWebBtn"] style:UIBarButtonItemStylePlain target:self action:@selector(forwardBtnTap:)];
+    _backBtn = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"backWebBtn"] style:UIBarButtonItemStylePlain target:self action:@selector(backBtnTap:)];
+    self.navigationItem.rightBarButtonItems = @[_actionBtn, _refreshBtn, _forwardBtn, _backBtn];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    if (self.rssWebView.isLoading) {
+        [self.rssWebView stopLoading];
+    }
+    
+    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,8 +55,56 @@
 
 #pragma mark - UIWebViewDelegate
 - (void)webViewDidStartLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    /* 戻る・進むボタンのタップ可否を制御 */
+    self.backBtn.enabled = self.rssWebView.canGoBack;
+    self.forwardBtn.enabled = self.rssWebView.canGoForward;
+    
+    /* navigationbarのitemを入れ替える */
+    NSMutableArray *changedBarButtonItemArray = [NSMutableArray arrayWithArray:[self.navigationItem rightBarButtonItems]];
+    [changedBarButtonItemArray replaceObjectAtIndex:1 withObject:_stopBtn];
+    self.navigationItem.rightBarButtonItems = changedBarButtonItemArray;
+    
+    _webViewLoads++;
 }
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
+    _webViewLoads--;
+    
+    if (_webViewLoads <= 0) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        
+        NSMutableArray *changedBarButtonItemArray = [NSMutableArray arrayWithArray:[self.navigationItem rightBarButtonItems]];
+        [changedBarButtonItemArray replaceObjectAtIndex:1 withObject:_refreshBtn];
+        self.navigationItem.rightBarButtonItems = changedBarButtonItemArray;
+    }
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    _webViewLoads--;
+    NSLog(@"error webView");
+}
+
+- (void)refreshBtnTap:(UIButton *)sender {
+    [self.rssWebView reload];
+}
+
+- (void)stopBtnTap:(UIButton *)sender {
+    [self.rssWebView stopLoading];
+    
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    
+    NSMutableArray *changedBarButtonItemArray = [NSMutableArray arrayWithArray:[self.navigationItem rightBarButtonItems]];
+    [changedBarButtonItemArray replaceObjectAtIndex:1 withObject:_refreshBtn];
+    self.navigationItem.rightBarButtonItems = changedBarButtonItemArray;
+}
+
+- (void)forwardBtnTap:(UIButton *)sender {
+    if (self.rssWebView.canGoForward) [self.rssWebView goForward];
+}
+
+- (void)backBtnTap:(UIButton *)sender {
+    if (self.rssWebView.canGoBack) [self.rssWebView goBack];
 }
 
 @end
